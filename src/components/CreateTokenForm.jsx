16624915d25da78fac8c10f899f469e95667d849
Alexis <mencoalexis@gmail.com>
2025-09-23 20:45:38 -0300

    commit

--- src/components/CreateTokenForm.jsx
+++ src/components/CreateTokenForm.jsx
@@ -27,7 +27,6 @@ export default function CreateTokenForm({ wallet, tonConnectUI }) {
     if (error) console.error(error);
   };
 
-          console.log('mi wallettttt ', walletAddress)
 
   const handleDeployed = ({ txHash, contractAddress }) => {
     setTxHash(txHash);
